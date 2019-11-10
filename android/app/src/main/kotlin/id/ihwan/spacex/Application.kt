55067e47b4ac05450ae015e4a2eaff652c822b1b
IhwanID <mynameisihwan@gmail.com>
2019-11-10 18:47:55 +0700

    setup firebase

---
+++ android/app/src/main/kotlin/id/ihwan/spacex/Application.kt
@@ -0,0 +1,16 @@
+package id.ihwan.spacex
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application: FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+    override fun registerWith(registry: PluginRegistry?) {
+        GeneratedPluginRegistrant.registerWith(registry)
+    }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:spacex/screen/dragons_screen.dart';
 import 'package:spacex/screen/launches_scree.dart';
 import 'package:spacex/screen/rockets_screen.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +11,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
   int _selectedIndex = 0;
+
+  Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data'];
+    }
+
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      final dynamic notification = message['notification'];
+    }
+
+    // Or do other work.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print('on message $message');
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('on resume $message');
+
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('on launch $message');
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.getToken().then((token){
+      print(token);
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
